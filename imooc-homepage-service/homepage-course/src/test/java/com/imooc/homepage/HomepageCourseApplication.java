b237431a4d8d0e59fbfd5d65a284362471f2b66f
wenyutun <wenyutun@163.com>
2020-01-04 13:46:22 +0800

    课程服务测试

---
+++ imooc-homepage-service/homepage-course/src/test/java/com/imooc/homepage/HomepageCourseApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wyt
+ * @description: 课程服务测试入口
+ * @date: 2020/1/2
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class HomepageCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomepageCourseApplication.class,args);
+    }
+
+}
---
+++ imooc-homepage-service/homepage-course/src/test/java/com/imooc/homepage/service/impl/CourseServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.imooc.homepage.service.impl;
+
+import com.imooc.homepage.HomepageCourseApplication;
+import com.imooc.homepage.dao.HomepageCourseDao;
+import com.imooc.homepage.entity.HomepageCourse;
+import com.imooc.homepage.service.CourseService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {HomepageCourseApplication.class},webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class CourseServiceImplTest {
+
+    @Autowired
+    private HomepageCourseDao courseDao;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Test
+    public void getCourseInfo() {
+
+    }
+
+    @Test
+    public void getCourseInfos() {
+    }
+
+    @Test
+    public void testCreatCourseInfo() {
+        HomepageCourse course = new HomepageCourse(
+                "JDK11新特性解读",
+                0,
+                "http://www/imooc.com",
+                "解读JDK11的版本新特新");
+        HomepageCourse course1 = new HomepageCourse(
+                "SpringBoot2.X实战",
+                1,
+                "http://www.immoc.com",
+                "SpringBoot2.X实战课程");
+
+        courseDao.saveAll(Arrays.asList(course,course1));
+    }
+
+
+}
