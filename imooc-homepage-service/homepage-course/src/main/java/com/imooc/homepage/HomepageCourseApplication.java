1026831ea12a91d572054cea07b142ccbbda3a81
wenyutun <wenyutun@163.com>
2020-01-12 17:02:56 +0800

    修改服务发现注解，新增config服务配置模块

--- imooc-homepage-service/homepage-course/src/main/java/com/imooc/homepage/HomepageCourseApplication.java
+++ imooc-homepage-service/homepage-course/src/main/java/com/imooc/homepage/HomepageCourseApplication.java
@@ -2,7 +2,7 @@ package com.imooc.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class HomepageCourseApplication {
 
     public static void main(String[] args) {
--- imooc-homepage-service/homepage-user/src/main/java/com/imooc/homepage/HomepageUserApplication.java
+++ imooc-homepage-service/homepage-user/src/main/java/com/imooc/homepage/HomepageUserApplication.java
@@ -3,7 +3,7 @@ package com.imooc.homepage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -15,7 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
 public class HomepageUserApplication {
