e47678a8db3e5998cd5c5819dbff165595a0e659
wenyutun <wenyutun@163.com>
2020-07-12 12:08:26 +0800

    新增统一配置模块

---
+++ homepage-config/src/main/java/com.imooc.homepage/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.imooc.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wyt
+ * @description: Config 配置中心启动类
+ * @date: 2020/1/12
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
